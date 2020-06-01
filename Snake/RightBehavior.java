605422532e302f3dea2c12f29c3c1cd6dbc680f8
LEE-SUMIN <37135336+LEE-SUMIN@users.noreply.github.com>
2020-06-01 10:03:22 +0900

    Revert "Revert "apply Template Method, Strategy Pattern""
    This reverts commit 866fffbebc41acb6cb79f3600c73c84e95c36c37.

---
+++ Snake/RightBehavior.java
@@ -0,0 +1,20 @@
+
+public class RightBehavior extends CheckTemplate implements SnakeMoveBehavior{
+
+	private Snake snake = Snake.get_snake();
+	private GameBoard gameBoard = GameBoard.get_board();
+	
+	@Override
+	public boolean checkMovement() {
+		if (!snake.moveRight()) { // Check to see if the Snake has run into itself.
+			return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public void action() {
+		check();
+		gameBoard.set_movement(Direction.RIGHT);
+	}
+}
